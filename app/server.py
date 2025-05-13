fd065f0db5ce079c982e26bf77387648602dbf4b
Yash <yashg@digi-prodigy.com>
2025-05-13 12:53:43 +0530

    Change

--- app/server.py
+++ app/server.py
@@ -33,5 +33,5 @@ def add_cors_headers(response):
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 7860))
+    port = int(os.environ.get("PORT", 8080))
     app.run(host="0.0.0.0", port=port)
