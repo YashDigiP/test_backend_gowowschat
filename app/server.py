b0537383719ce4cb619c31175ef03632323fb8de
Impacterniraj <niraj@impacterpathway.com>
2025-05-15 12:30:00 +0530

    access origin

--- app/server.py
+++ app/server.py
@@ -26,7 +26,7 @@ register_routes(app)
 
 @app.after_request
 def add_cors_headers(response):
-    response.headers["Access-Control-Allow-Origin"] = "http://localhost:5173"
+    response.headers["Access-Control-Allow-Origin"] = "http://localhost:8080"
     response.headers["Access-Control-Allow-Headers"] = "Content-Type,Authorization"
     response.headers["Access-Control-Allow-Methods"] = "GET,POST,OPTIONS"
     return response
