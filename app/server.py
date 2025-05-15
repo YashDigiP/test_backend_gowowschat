5304b304081c6bda48e933cdd5a84b45c57db23e
Impacterniraj <niraj@impacterpathway.com>
2025-05-15 12:40:19 +0530

    Added all the origins

--- app/server.py
+++ app/server.py
@@ -14,7 +14,8 @@ from routes import register_routes
 
 
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "http://localhost:8080"}}, supports_credentials=True)
+CORS(app, resources={r"/*": {"origins": ["http://localhost:8080", "http://localhost:5173", "https://gowows-web-252628919239.asia-south1.run.app"]}}, supports_credentials=True)
+
 
 # 🔐 Add JWT config
 app.config["JWT_SECRET_KEY"] = "super-secret"  # TODO: change this before production!
@@ -26,7 +27,7 @@ register_routes(app)
 
 @app.after_request
 def add_cors_headers(response):
-    response.headers["Access-Control-Allow-Origin"] = "http://localhost:8080"
+    response.headers["Access-Control-Allow-Origin"] = ["http://localhost:8080", "https://gowows-web-252628919239.asia-south1.run.app", "http://localhost:5173"]
     response.headers["Access-Control-Allow-Headers"] = "Content-Type,Authorization"
     response.headers["Access-Control-Allow-Methods"] = "GET,POST,OPTIONS"
     return response
@@ -34,5 +35,5 @@ def add_cors_headers(response):
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 7860))
+    port = int(os.environ.get("PORT", 8080))
     app.run(host="0.0.0.0", port=port, debug=True)
